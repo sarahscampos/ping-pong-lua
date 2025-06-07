2186fd8e7acf66fb7cdf48e158a5e102a2605f0e
Sarah Campos <sarahstephanycampos@gmail.com>
2025-06-07 12:22:05 -0300

    feat: vitoria update

--- main.lua
+++ main.lua
@@ -44,7 +44,7 @@ function love.load()
     
     --fonte retro
     smallFont = love.graphics.newFont("font.ttf", 8)
-
+    largeFont = love.graphics.newFont("font.ttf", 16)
     scoreFont = love.graphics.newFont("font.ttf", 32)
 
     love.graphics.setFont(smallFont)
@@ -56,6 +56,9 @@ function love.load()
     })
     player1Score = 0
     player2Score = 0
+
+    -- comeca com 1, mas depois e determinado pelo jogador que pontuou
+    servingPlayer = 1
     
     -- inicializa as raquetes
     player1 = Paddle(10, 30, 5, 20)
@@ -67,10 +70,20 @@ function love.load()
     -- estado do jogo
     gameState = "start"
 
+    
+
 end
 
 function love.update(dt)
-    if gameState == "play" then
+    -- antes de mudar para play, inicializa a velocidade da bola baseada no jogador que pontuou por ultimo
+    if gameState == "serve" then
+        ball.dy = math.random(-50,50)
+        if servingPlayer == 1 then
+            ball.dx = math.random(140, 200)
+        else
+            ball.dx = -math.random(140, 200)
+        end
+    elseif gameState == "play" then
         -- detecta a colisao da bola com as raquetes e inverte o dx se verdadedeiro alem de aumentar um pouco sua velocidade
         if ball:collides(player1) then
             ball.dx = -ball.dx * 1.05
@@ -82,16 +95,16 @@ function love.update(dt)
                 ball.dy = math.random(10, 150)
             end
         end
-            if ball:collides(player2) then
-                ball.dx = -ball.dx * 1.05
-                ball.x = player2.x - 4 -- evita que detecte multiplas colisoes
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.05
+            ball.x = player2.x - 4 -- evita que detecte multiplas colisoes
 
-                -- mantem a direcao e sentido da bola
-                if ball.dy < 0 then
-                    ball.dy = -math.random(10, 150)
-                else
-                    ball.dy = math.random(10, 150)
-                end
+            -- mantem a direcao e sentido da bola
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
         end
 
         -- detecta limites superior e inferior (eixo y) da tela e inverte o dy se colidir
@@ -105,20 +118,32 @@ function love.update(dt)
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
         end
-
-    end
-
-    -- se alcanca os limites esquerdo ou direito da tela, reseta a posicao da bola e atualiza o placar
-    if ball.x < 0 then
-        player2Score = player2Score + 1
-        ball:reset()
-        gameState = "serve"
-    end
-
-    if ball.x > VIRTUAL_WIDTH then
-        player1Score = player1Score + 1
-        ball:reset()
-        gameState = "serve"
+        
+        -- se alcanca os limites esquerdo ou direito da tela, reseta a posicao da bola e atualiza o placar
+        if ball.x < 0 then
+            servingPlayer = 1
+            player2Score = player2Score + 1
+            if player2Score == 3 then
+                winningPlayer = 2
+                gameState = "done"
+            else
+                ball:reset()
+                gameState = "serve"
+            end
+        end
+    
+        if ball.x > VIRTUAL_WIDTH then
+            servingPlayer = 2
+            player1Score = player1Score + 1
+    
+            if player1Score == 3 then
+                winningPlayer = 1
+                gameState = "done"
+            else
+                ball:reset()
+                gameState = "serve"
+            end
+        end
     end
 
     -- movimento do jogador 1, no eixo Y pra cima e negativo e para baixo e positivo
@@ -154,13 +179,28 @@ end
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
-    elseif key == "enter" or key == "return" then
-        if gameState == "start" then
-            gameState = "play"
-        else
-            gameState = "start"
-     
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
+            gameState = 'play'
+        elseif gameState == 'done' then
+            -- game is simply in a restart phase here, but will set the serving
+            -- player to the opponent of whomever won for fairness!
+            -- reset scores to 0
+            player1Score = 0
+            player2Score = 0
+
             ball:reset()
+            
+            gameState = 'serve'
+
+            -- decide serving player as the opposite of who won
+            if winningPlayer == 1 then
+                servingPlayer = 2
+            else
+                servingPlayer = 1
+            end
         end
     end
 end
@@ -172,19 +212,61 @@ function love.draw()
     --cor da tela
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
-    love.graphics.setFont(smallFont)
-    love.graphics.printf(
-        "PONG",
-        0,
-        20,
-        VIRTUAL_WIDTH,
-        "center"
-    )
+    if gameState == 'start' then 
+        love.graphics.setFont(smallFont)
+        love.graphics.printf(
+            "PONG",
+            0,
+            20,
+            VIRTUAL_WIDTH,
+            "center"
+        )
+        love.graphics.printf(
+            "Pressione Enter para iniciar!",
+            0,
+            30,
+            VIRTUAL_WIDTH,
+            "center"
+        )
+    elseif gameState == 'serve' then
+        love.graphics.setFont(smallFont)
+        love.graphics.printf(
+            "Saque do Player " .. tostring(servingPlayer) .. "!",
+            0,
+            10,
+            VIRTUAL_WIDTH,
+            "center"
+        )
+        love.graphics.printf(
+            "Pressione Enter para sacar!",
+            0,
+            20,
+            VIRTUAL_WIDTH,
+            "center"
+        )
 
-    love.graphics.setFont(scoreFont)
-    --renderiza placar
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    elseif gameState == "play" then
+        -- nao faz nada
+    elseif gameState == "done" then
+        love.graphics.setFont(largeFont)
+        love.graphics.printf(
+            "Player " .. tostring(winningPlayer) .. " ganhou!",
+            0,
+            10,
+            VIRTUAL_WIDTH,
+            "center"
+        )
+        love.graphics.setFont(smallFont)
+        love.graphics.printf(
+            "Pressione Enter para jogar novamente!",
+            0, 
+            30,
+            VIRTUAL_WIDTH,
+            "center"
+        )
+    end
+
+    displayScore()
 
     --renderiza primeira raquete (esquerda)
     player1:render()
@@ -204,4 +286,11 @@ function displayFPS()
     love.graphics.setFont(smallFont)
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 10, 10)
+end
+
+function displayScore()
+    love.graphics.setFont(scoreFont)
+    --renderiza placar
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 end
